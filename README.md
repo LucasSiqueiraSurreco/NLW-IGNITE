f47fadd9e1b9cddd21652ed58518ba17a9b71f29
LucasSiqueiraSurreco <lucas.siqueira.15@live.com.pt>
2022-09-16 21:09:25 -0300

    projeto finalizado

---
+++ README.md
@@ -0,0 +1,44 @@
+# NLW eSports Ignite
+![preview](./.github/preview-1.png)
+
+## 💠 API Backend
+
+- Typescript
+- Vite
+- NodeJS
+- Express
+- Prisma
+- SQLite
+
+--
+## 🎮 Web front-end
+
+- Typescript
+- Vite
+- ReactJS
+- Tailwind CSS
+- Radix UI
+- Axios
+- Phosphor Icons
+
+--
+## 📱 Mobile
+
+- Typescript
+- ReactNative
+- Expo
+- Tailwind CSS
+- Radix UI
+- Axios
+- Phosphor Icons
+
+--
+## 🖼️ Galleery
+
+![preview](./.github/preview-2.png)
+
+
+![preview](./.github/preview-4.png)
+
+![preview](./.github/preview-5.png)
+![preview](./.github/preview-6.png)
---
+++ mobile/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react'
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+import * as Notifications from 'expo-notifications'
+import { Subscription } from 'expo-modules-core'
+
+import { Routes } from './src/routes'
+import { Background } from "./src/components/Background"
+import { Loading } from './src/components/Loading'
+
+import './src/services/notificationConfigs'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
+
+export default function App() {
+
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
+  )
+}
